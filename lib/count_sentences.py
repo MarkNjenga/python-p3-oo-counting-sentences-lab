9317ed302f5645e2cd44bedef24765184405aa3e
Mark Njenga <marknjenga25@gmail.com>
2024-09-07 12:30:12 +0300

    "Added implementation to MyString class with value property, sentence type checks, and sentence counting method."

--- lib/count_sentences.py
+++ lib/count_sentences.py
@@ -1,4 +1,31 @@
-#!/usr/bin/env python3
-
 class MyString:
-  pass
+    def __init__(self, value=""):
+        self.value = value
+
+    @property
+    def value(self):
+        return self._value
+
+    @value.setter
+    def value(self, value):
+        if not isinstance(value, str):
+            print("The value must be a string.")
+            self._value = ""
+        else:
+            self._value = value
+
+    def is_sentence(self):
+        return self.value.endswith(".")
+
+    def is_question(self):
+        return self.value.endswith("?")
+
+    def is_exclamation(self):
+        return self.value.endswith("!")
+
+    def count_sentences(self):
+        sentences = 0
+        for i in range(len(self.value)):
+            if self.value[i] in ".!?" and (i == len(self.value) - 1 or self.value[i + 1] != self.value[i]):
+                sentences += 1
+        return sentences
